e7ab38f73ac0ae9d3e153d1ab7518827b2b3ab1b
KLASS Daniel <daniel.klass@soprasteria.com>
2020-03-27 17:32:51 +0100

    Change to monika

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
+buzz = ('monika', 'daniel', 'name')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
